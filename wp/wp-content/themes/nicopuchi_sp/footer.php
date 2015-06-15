aac1d547e8162ffdaf75f067eb195d14b354416f
Masayoshi Uematsu <uety0211@gmail.com>
2015-06-15 12:42:36 +0900

    sp theme

---
+++ wp/wp-content/themes/nicopuchi_sp/footer.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * フッタ
+ **/
+?>
+<?php include(dirname(__FILE__).'/../../../../common/inc/sp/footer.html'); ?>
+<script type="text/javascript" src="/common/js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="/common/js/selectivizr-min.js"></script>
+<script type="text/javascript" src="/common/js/script_sp.js"></script>
+<script type="text/javascript" src="/common/js/jquery.bxslider.js"></script>
+<script type="text/javascript" src="/common/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript">
+$(function() {
+	var slider = $('.bxslider').bxSlider({
+		auto: true,
+		infinite: true,
+		pause: 5000,
+		speed: 1000,
+		mode: 'horizontal',
+		prevText: '<',
+		nextText: '>',
+		pager: true,
+		//easing: 'easeOutBounce',
+		pagerCustom: '.bx-pager'
+	});
+});
+</script>
+<script type="text/javascript" src="/common/js/slick.js"></script>
+<script>
+$("document").ready(function(){
+	$('#blogsupporter .slider-type02').slick({
+		infinite: true,
+		autoplay:true,
+		dots:true,
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+	$('#puchiblog .slider-type02,#readersblog .slider-type02').slick({
+		infinite: true,
+		autoplay:true,
+		dots:true,
+		slidesToShow: 2,
+		slidesToScroll: 1
+	});
+});
+</script>
+<script type="text/javascript" src="/common/js/jquery.heightLine.js"></script>
+<script>
+	 $(".publog").heightLine();
+</script>
+</body>
+</html>
---
+++ wp/wp-content/themes/nicopuchi_sp/header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * ヘッダー
+ */
+?><!DOCTYPE HTML>
+<!--[if IE 7 ]><html class="ie7" lang="ja"><![endif]-->
+<!--[if IE 8 ]><html class="ie8" lang="ja"><![endif]-->
+<!--[if IE 9 ]><html class="ie9" lang="ja"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="ja">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="robots" content="index,noydir,noodp">
+<meta http-equiv="imagetoolbar" content="no">
+<meta name="description" content="*****">
+<meta name="keywords" content="*****">
+<meta name="copyright" content="*****">
+<meta name="format-detection" content="telephone=no">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="*****">
+<meta property="og:url" content="http://www.**">
+<meta property="og:image" content="http://www.**">
+<meta property="og:title" content="****">
+<meta property="og:description" content="****">
+<meta name="viewport" content="width=device-width;target-densitydpi=medium-dpi, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<title><?php wp_title('｜', true, 'right'); ?></title>
+<link rel="stylesheet" href="/common/css/style_sp.css">
+<link rel="stylesheet" href="/common/css/slick_sp.css">
+<link rel="stylesheet" media="print" href="/common/css/print.css">
+</head>
+<body id="pagetop">
+<?php include(dirname(__FILE__).'/../../../../common/inc/sp/header.html'); ?>
