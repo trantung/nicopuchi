5cbd996638ba56bab5709f8b18efe4d4537ecdc1
Masayoshi Uematsu <uety0211@gmail.com>
2015-06-15 16:36:56 +0900

    add template file

---
+++ wp/wp-content/themes/nicopuchi_sp/category/category-nico_cover.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * 表紙コレクション
+ */
+?>
+
+<article id="main">
+
+    <ul class="dir-path cFix">
+    	<li><a href="">TOP</a>></li>
+    	<li>ページタイトル</li>
+    </ul>
+
+    <div class="module-type01">
+    	<div class="module-body bg-type02">
+    		<div class="module-head inner10">
+    			<h2 class="module-head-ttl pb10"><img class="full" src="/common/img/sp/07/ttl01.png" alt="表紙コレクション"></h2>
+    			<div id="cover">
+    				<div id="newest">
+    					<div id="newest_cover">
+    						<div id="icn_new"><img src="../common/img/sp/07/icn_new.png" width="90" height="90" alt="最新号"></div>
+    						<a href="#modal1" class="modalLink">
+    							<div class="cover_ph"><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></div>
+    							<div class="closeup"><img src="../common/img/sp/07/icn_zoom.png" width="20" height="20">大きい画像を見る</div>
+    						</a>
+    					</div>
+    					<div class="cover_ph_sub"><a href="#modal2" class="modalLink"><img class="full" src="../common/img/sp/07/sample01.jpg"></a></div>
+    					<div class="txtbx">
+    						<p class="col_b">キーワードは5コ!!! </p>
+    						<p class="col_o">おしゃれJS的　サマートレンドを先ドリ♪ </p>
+    						<p class="col_r">夏ファッションHIT予報！</p>
+    					</div>
+    					<div class="cover_ph_sub"><a href="#modal3" class="modalLink"><img class="full" src="../common/img/sp/07/sample02.jpg"></a></div>
+    					<div class="txtbx">
+    						<p class="col_o">肌タイプ別にピッタリなケア方法＆アイテムをレクチャー </p>
+    						<p class="col_r">夏肌てってーケアスキンケア入門</p>
+    					</div>
+    					<div class="cover_ph_sub"><a href="#modal4" class="modalLink"><img class="full" src="../common/img/sp/07/sample03.jpg"></a></div>
+    					<div class="txtbx">
+    						<p class="col_p">バッグ＆リュックの中身もみせちゃう!! </p>
+    						<p class="col_g">6年生修学旅行コーデ＆4・5年生遠足コーデ</p>
+    					</div>
+    					<div class="txtbx">
+    						<p class="tx_c"><img src="../common/img/sp/07/201506furoku.png" width="132" height="285"></p>
+    					</div>
+    				</div>
+    				<div id="timeline_issue">
+    					<ul class="timeline-inner">
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年6月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					<li class="item"><a href="">
+    						<dl>
+    							<dt class="date">2015年4月号</dt>
+    							<dd><img class="full" src="/common/img/sp/07/local07_cover_thumb01.jpg" alt=""></dd>
+    						</dl>
+    						<span class="read_btn">立ち読み</span></a></li>
+    					</ul>
+    				<!--/#timeline--></div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <!--/.module-type01-->
+<!--/#main--></article>
