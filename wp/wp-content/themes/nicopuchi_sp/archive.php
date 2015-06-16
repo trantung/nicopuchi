863c93c25c65f33b670e52962d8bf497ab49f5d5
Masayoshi Uematsu <uety0211@gmail.com>
2015-06-16 13:14:40 +0900

    edit category page

--- wp/wp-content/themes/nicopuchi_sp/archive.php
+++ wp/wp-content/themes/nicopuchi_sp/archive.php
@@ -9,7 +9,15 @@ get_header();
 
 if (is_post_type_archive('puchinew'))
 {
-    include(TEMPLATEPATH . '/category/category-puchinew.php');
+	include(TEMPLATEPATH . '/category/category-puchinew.php');
+}
+elseif (is_post_type_archive('nico_profile'))
+{
+	include(TEMPLATEPATH . '/category/category-nico_profile.php');
+}
+elseif (is_post_type_archive('nico_cover'))
+{
+	include(TEMPLATEPATH . '/category/category-nico_cover.php');
 }
 
 
