b2b99f5d843a65e1d4f7e9a1c3ce0cc9158910af
Mori <morimorimo0000@gmail.com>
2015-06-16 19:23:42 +0900

    PC コメント修正

--- wp/wp-content/themes/nicopuchi/comments.php
+++ wp/wp-content/themes/nicopuchi/comments.php
@@ -18,11 +18,17 @@
         </ul>
     </div>
     <?php
+    $logged_in_as = '<p class="logged-in-as">'
+                    . sprintf(__('%2$sとしてログインしています。'
+                    . ' <a href="%3$s">ログアウト</a>'),
+                    admin_url('profile.php'),
+                    $user_identity,
+                    wp_logout_url(apply_filters('the_permalink', get_permalink($post_id)))) . '</p>';
     $args = array(
         'must_log_in' => null,
-        'logged_in_as' => null,
-        'comment_notes_before'  => null,
-        'comment_notes_after'  => null,
+        'logged_in_as' => $logged_in_as,
+        'comment_notes_before' => null,
+        'comment_notes_after' => null,
     );
     comment_form($args);
     ?>
