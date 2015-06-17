c41692ca32c5d8fc991148db6cc37869e341f8ce
Mori <morimorimo0000@gmail.com>
2015-06-17 09:27:16 +0900

    PC ページテンプレ修正

--- wp/wp-content/themes/nicopuchi/page/page-base.php
+++ wp/wp-content/themes/nicopuchi/page/page-base.php
@@ -9,17 +9,19 @@
 <div id="contents">
     <div id="main">
         <?php my_breadcrumbs(); ?>
-
-        <div class="module-type01 bg-type03 pb60">
-            <div class="module-body">
-                <?php if (have_posts()) : ?>
-                    <?php the_post(); ?>
-                    <?php the_title(); ?>
-                    <?php the_content(); ?>
-                <?php endif; ?>
-            </div>
+        <div class="module-type01 bg-type02 inner15">
+            <?php if (have_posts()) : ?>
+                <?php the_post(); ?>
+                <div class="module-head">
+                    <h2 class="tx_c mb15"><?php the_title(); ?></h2>
+                </div>
+                <div class="module-body">
+                    <div class="about-area inner15">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
             <!--/.module-type01--></div>
         <!--/#main--></div>
     <!--/#contents--></div>
-
 
