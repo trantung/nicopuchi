7b473c72b9b07d72b6f474fe9eaf1b86df8fe936
Masayoshi Uematsu <uety0211@gmail.com>
2015-06-17 12:30:01 +0900

    add 404.php

---
+++ wp/wp-content/themes/nicopuchi_sp/404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 404ページ
+ */
+?>
+<?php get_header(); ?>
+
+	<article id="main">
+		<div class="module-type01">
+			<div class="module-head">
+				<h2 class="module-head-ttl mb10">お探しのページが見つかりませんでした</h2>
+			</div>
+			<div>
+				<p>お探しのページが見つかりませんでした。<br>
+					このページは、削除もしくはURLが変更された可能性がございます。<br>
+				</p>
+			</div>
+		</div>
+	</article>
+
+<?php get_footer(); ?>
