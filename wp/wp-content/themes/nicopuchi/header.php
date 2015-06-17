19ac5c69fa0aa1583bc40ed3564f66f7de451a6b
Mori <morimorimo0000@gmail.com>
2015-06-17 17:46:28 +0900

    PC ヘッダーフッターをincフォルダからの読み込みに変更

--- wp/wp-content/themes/nicopuchi/header.php
+++ wp/wp-content/themes/nicopuchi/header.php
@@ -35,28 +35,6 @@
 
 
 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.3";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-<div id="header">
-    <div class="header-inner">
-        <h1><a href="/"><img src="/common/img/pc/img_logo.png" alt="ニコ☆プチ" width="213" height="64"></a></h1>
-        <ul id="gnav">
-            <li><a href="/support/" class="fdb"><img src="/common/img/pc/gnav01.png" alt="ニコ☆プチ読者ブログサポーター" width="151" height="28"></a></li>
-            <li><a href="/puchinew/" class="fdb"><img src="/common/img/pc/gnav02.png" alt="プチニュー10" width="130" height="28"></a></li>
-            <li><a href="http://ameblo.jp/nicopuchi-staff/" class="fdb" target="_blank"><img src="/common/img/pc/gnav03.png" alt="ニコ☆プチ編集部ブログ" width="130" height="28"></a></li>
-            <li><a href="/nico_cover/" class="fdb"><img src="/common/img/pc/gnav04.png" alt="ニコプチ表紙" width="129" height="28"></a></li>
-            <li><a href="/nico_profile/" class="fdb"><img src="/common/img/pc/gnav05.png" alt="プチモ一覧" width="130" height="28"></a></li>
-            <li><a href="/blog/" class="fdb"><img src="/common/img/pc/gnav06.png" alt="プチモブログ" width="130" height="28"></a></li>
-            <li><a href="/puchisna/" class="fdb"><img src="/common/img/pc/gnav07.png" alt="プチ撮影日記" width="129" height="28"></a></li>
-        <!--/#gnav--></ul>
-    </div>
-<!--/#header--></div>
+<?php include(dirname(__FILE__).'/../../../../common/inc/pc/header.html'); ?>
 
 
